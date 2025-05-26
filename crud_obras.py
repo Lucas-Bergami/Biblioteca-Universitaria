bcf23863dc5651695a419f26acd81203be8a94eb
Lucas-Bergami <lucasbergami5@gmail.com>
2025-05-26 17:41:13 -0300

    [ADM02]: Adicionada função para atualização de dados de uma obra no banco de dados

--- crud_obras.py
+++ crud_obras.py
@@ -27,3 +27,18 @@ def listar_obras():
     finally:
         cursor.close()
         conexao.close()
+
+def atualizar_obra(id_obra, titulo, ano_publicacao, edicao, id_editora):
+    conexao = conectar()
+    cursor = conexao.cursor()
+    try:
+        sql = "UPDATE Obras SET titulo = %s, ano_publicacao = %s, edicao = %s, id_editora = %s WHERE id_obra = %s"
+        valores = (titulo, ano_publicacao, edicao, id_editora, id_obra)
+        cursor.execute(sql, valores)
+        conexao.commit()
+        print("Obra atualizada com sucesso!")
+    except mysql.connector.Error as err:
+        print(f"Erro ao atualizar obra: {err}")
+    finally:
+        cursor.close()
+        conexao.close()
